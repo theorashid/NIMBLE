314b53cb732d505639e42aa977bf4afc4226a9ba
Theo Rashid <theoaorashid@gmail.com>
2020-02-25 22:49:59 +0000

    added data simulation code

--- Extra/LE_test.R
+++ Extra/LE_test.R
@@ -6,6 +6,8 @@ source("period_life_table.R")
 ##EXAMPLE
 #rough estimated death rates for 18 age groups
 mxE <- exp(-5.5 + c(-1,-3,-2.9,-1.8,-1.7,-1.7,-1.6,-1.4,-.8,-.2,.3,1.0,1.5,1.9,2.4,2.8,3.4,3.8))
+# Pops <- c(rep(100000,10),rep(1000,8))
+# rpos(n=18, mxE*Pops)
  
 #function to give table
 test <- PeriodLifeTable(mx=mxE,age=seq(0, 85, 5),ax=rep(NA,18),full.table=TRUE)
